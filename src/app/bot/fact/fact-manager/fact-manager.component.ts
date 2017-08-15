39ff597b976a7a8b6ce7c6cd4c48851e72cf5979
Robert Schumann <robert.schumann@unic.com>
2017-08-15 11:09:13 +0200

    added sockets to bot stuff

--- src/app/bot/fact/fact-manager/fact-manager.component.ts
+++ src/app/bot/fact/fact-manager/fact-manager.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { RestService, RestResource } from '../../../rest'
+import { SocketService } from '../../../socket/socket.service'
+
 import { Fact } from '../fact.type'
 
 @Component({
@@ -12,13 +14,34 @@ export class FactManagerComponent implements OnInit {
   private factResource: RestResource<Fact>
   private facts: Fact[] = []
 
-  constructor(private rest: RestService) {
+  constructor(private rest: RestService, private socket: SocketService) {
     this.factResource = rest.createResource('/api/rest/uselessfact')
 
     this.fetchFacts()
   }
 
+  trackBy(index, fact) { return fact._id }
+
+  addFact() {
+    this.factResource.post({}).subscribe(() => {})
+  }
+
+  updateFact(fact: Fact) {
+    this.factResource.update(fact._id, fact).subscribe(() => {})
+  }
+
+  deleteFact(fact: Fact) {
+    this.factResource.delete(fact._id).subscribe(() => {})
+  }
+
   ngOnInit() {
+    this.socket.on('UselessFact created', () => this.fetchFacts())
+    this.socket.on('UselessFact deleted', () => this.fetchFacts())
+    this.socket.on('UselessFact updated', () => this.fetchFacts())
+  }
+
+  ngOnDestroy() {
+
   }
 
   fetchFacts() {
--- src/app/bot/reaction/reaction-manager/reaction-manager.component.ts
+++ src/app/bot/reaction/reaction-manager/reaction-manager.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 import { RestService, RestResource } from '../../../rest'
+import { SocketService } from '../../../socket/socket.service'
 import { Reaction } from '../reaction.type'
+
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
 
 @Component({
   selector: 'app-reaction-manager',
@@ -12,17 +16,38 @@ export class ReactionManagerComponent implements OnInit {
   private reactionResource: RestResource<Reaction>
   private reactions: Reaction[] = []
 
-  constructor(private rest: RestService) {
+  constructor(private rest: RestService, private socket: SocketService) {
     this.reactionResource = rest.createResource('/api/rest/botreaction')
 
     this.fetchReactions()
   }
 
+  trackBy(index, reaction) { return reaction._id }
+
   ngOnInit() {
+    this.socket.on('BotReaction created', () => this.fetchReactions())
+    this.socket.on('BotReaction deleted', () => this.fetchReactions())
+    this.socket.on('BotReaction updated', () => this.fetchReactions())
   }
 
+  addReaction() {
+    this.reactionResource.post({}).subscribe(() => {})
+  }
+
+  updateReaction(reaction: Reaction) {
+    this.reactionResource.update(reaction._id, reaction).subscribe(() => { console.log('meow') })
+  }
+
+  deleteReaction(reaction: Reaction) {
+    this.reactionResource.delete(reaction._id).subscribe(() => {})
+  }
+
+  reactionQuery = undefined
+
   fetchReactions() {
-    this.reactionResource.query({}).subscribe(facts => {
+    if(!this.reactionQuery) this.reactionQuery = this.reactionResource.query({})
+
+    this.reactionQuery.subscribe(facts => {
       this.reactions = facts
     })
   }
